eb03a16e1ba617b378a790b860e99621949df8ea
Calin <nilac8991@gmail.com>
2017-04-05 16:41:01 +0200

    Reboot QS Tile: Make it better - Added options to make Soft Reboot and SystemUI Reboot - Code re-organization - Vectorized used drawables
    Signed-off-by: Calin <nilac8991@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 Slimroms
+ * Copyright (C) 2017 Xperia Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,17 +17,28 @@
 
 package com.android.systemui.qs.tiles;
 
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.ServiceManager;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.RemoteException;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.util.benzo.Helpers;
+
+import android.util.Log;
 
 public class RebootTile extends QSTile<QSTile.BooleanState> {
 
-    private boolean mRebootToRecovery = false;
+    //1 Normal Reboot
+    //2 Reboot to Recovery
+    //3 Soft Reboot
+    //4 Reboot SystemUI
+    private int mChoiceNumber = 1;
 
     public RebootTile(Host host) {
         super(host);
@@ -44,21 +56,70 @@ public class RebootTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     protected void handleClick() {
-        mRebootToRecovery = !mRebootToRecovery;
-        refreshState();
+        if(mChoiceNumber == 4){
+            mChoiceNumber = 1;
+            refreshState();
+        }
+        else{
+            mChoiceNumber++;
+            refreshState();
+        }
+    }
+
+    private static void doSystemUIReboot() {
+        Helpers.restartSystemUI();
+    }
+
+    private void doSoftReboot() {
+        try {
+            final IActivityManager am = ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
+            
+            if (am != null)
+                am.restart();
+
+        } catch (RemoteException e) {
+            //Don't need anything from here
+        }
     }
 
     @Override
     protected void handleLongClick() {
-        mHost.collapsePanels();
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            public void run() {
-                PowerManager pm =
-                    (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-                pm.reboot(mRebootToRecovery ? "recovery" : "");
-            }
-        }, 500);
+        PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        String mReason;
+        mHost.collapsePanels();
+        
+        switch(mChoiceNumber){
+            case 1:
+                mReason="REBOOT";
+                handler.postDelayed(new Runnable() {
+                    public void run(){
+                        pm.reboot(mReason);
+                    }
+                }, 500);
+            break;
+            case 2:
+                mReason="recovery";
+                handler.postDelayed(new Runnable() {
+                    public void run(){
+                        pm.reboot(mReason);
+                    }
+                    }, 500);
+            break;
+            case 3:
+                handler.postDelayed(new Runnable() {
+                    public void run(){
+                        doSoftReboot();
+                    }
+                }, 500);
+            break;
+            case 4:
+                handler.postDelayed(new Runnable() {
+                    public void run(){
+                        doSystemUIReboot();
+                    }
+                }, 500);
+        }
     }
 
     @Override
@@ -73,16 +134,28 @@ public class RebootTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        if (mRebootToRecovery) {
-            state.label = mContext.getString(R.string.quick_settings_reboot_recovery_label);
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_recovery);
-            state.contentDescription =  mContext.getString(
-                    R.string.quick_settings_reboot_recovery_label);
-        } else {
-            state.label = mContext.getString(R.string.quick_settings_reboot_label);
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
-            state.contentDescription =  mContext.getString(
-                    R.string.quick_settings_reboot_label);
+       
+        switch(mChoiceNumber){
+            case 1:
+                state.label = mContext.getString(R.string.quick_settings_reboot_label);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
+                state.contentDescription =  mContext.getString(R.string.quick_settings_reboot_label);
+            break;
+            case 2:
+                state.label = mContext.getString(R.string.quick_settings_reboot_recovery_label);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_recovery);
+                state.contentDescription =  mContext.getString(R.string.quick_settings_reboot_recovery_label);
+            break;
+            case 3:
+                state.label = mContext.getString(R.string.quick_settings_soft_reboot_label);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
+                state.contentDescription =  mContext.getString(R.string.quick_settings_soft_reboot_label);
+            break;
+            case 4:
+                state.label = mContext.getString(R.string.quick_settings_systemui_reboot);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_systemui);
+                state.contentDescription =  mContext.getString(R.string.quick_settings_systemui_reboot);
+            break;
         }
     }
 
